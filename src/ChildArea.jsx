d301805999fea617a22522f12ef77418684ee20e
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-24 17:15:42 +0900

    memo, useCallback, useMemo

---
+++ src/ChildArea.jsx
@@ -0,0 +1,28 @@
+import React, { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+
+  const data = [...Array(2000).keys()];
+  console.log("ChildAreaが再レンダリングされた!");
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>Child Component</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import "./styles.css";
+import React from "react";
+import ReactDom from "react-dom";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+import App from "./App";
 
-  <p>テスト</p>
-</div>
-`;
+ReactDom.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
